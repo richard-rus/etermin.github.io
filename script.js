ce884e5da6bd32eeb808a3e7e8346c6f3eaef27d
richard-rus <155675172+richard-rus@users.noreply.github.com>
2026-02-18 11:30:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+body {
+    margin: 0;
+    font-family: 'Segoe UI', sans-serif;
+    background: linear-gradient(135deg, #1b5e20, #66bb6a);
+}
+
+header {
+    text-align: center;
+    padding: 40px;
+    color: white;
+}
+
+.page {
+    display: none;
+    background: white;
+    padding: 40px;
+    max-width: 1000px;
+    margin: auto;
+    border-radius: 15px;
+}
+
+.active {
+    display: block;
+}
